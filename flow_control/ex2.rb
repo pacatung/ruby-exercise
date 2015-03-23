efb23cac77cdedaa41e52077911a671115072273
BigBoss <pacatung@gmail.com>
2015-03-23 21:34:29 +0800

    ex2

---
+++ flow_control/ex2.rb
@@ -0,0 +1,23 @@
+
+
+def changetoupcase(word)
+
+  if word.length >=10
+    puts "The word's length longer than 10"
+
+  else
+    puts word.upcase
+  end
+
+
+end
+
+
+changetoupcase('asdkfasdfjasdfj')
+changetoupcase('asdfklj')
+
+puts 'asdfklj'
+puts '\"asdfklj\"'
+puts "\nasdfklj\""
+
+
