ac4fcb95f1b1dfc2504be4091394508d4de5bc60
BigBoss <pacatung@gmail.com>
2015-03-23 18:43:57 +0800

    ex3

---
+++ basic/ex3.rb
@@ -0,0 +1,11 @@
+movies = { aaa: 2345,
+           bbb: 2354,
+           ccc: 2222,
+           ddd: 2033,
+           eee: 1999 }
+
+puts movies :aaa
+puts movies :bbb
+puts movies :ccc
+puts movies :ddd
+puts movies :eee
