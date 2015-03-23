f6bc8bf5170da27427869432fed1e30219e0cb90
BigBoss <pacatung@gmail.com>
2015-03-24 00:35:19 +0800

    ex3.rb

---
+++ flow_control/ex3.rb
@@ -0,0 +1,17 @@
+def if_else(n)
+  if n <= 50
+    puts "#{n} is between 0 and 50."
+  elsif n>50 && n<=100
+    puts "#{n} is between 50 and 100."
+  elsif n>100
+
+    puts "#{n} is above 100"
+      end
+end
+
+
+
+
+if_else(49)
+if_else(99)
+if_else(134)
