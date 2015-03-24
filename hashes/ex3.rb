02c31e9d072aa77041fac320baa834b91ef7e135
BigBoss <pacatung@gmail.com>
2015-03-24 18:55:22 +0800

    ex3.rb

---
+++ hashes/ex3.rb
@@ -0,0 +1,12 @@
+h={key1: "value1", key2: "value2", key3:"value3"}
+
+puts h.keys
+puts h.values
+
+
+#======tealeaf
+#opposites = {positive: "negative", up: "down", right: "left"}
+#
+#opposites.each_key { |key| puts key }
+#opposites.each_value { |value| puts value }
+#opposites.each { |key, value| puts "The opposite of #{key} is #{value}" }
