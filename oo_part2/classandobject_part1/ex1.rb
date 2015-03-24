d8bd63e8829ebb028c21516f868fcdfc74a8bab0
BigBoss <pacatung@gmail.com>
2015-03-24 21:42:53 +0800

    first modify

--- oo_part2/classandobject_part1/ex1.rb
+++ oo_part2/classandobject_part1/ex1.rb
@@ -1,18 +1,26 @@
+# tealeaf http://www.gotealeaf.com/books/oo_ruby
 class MyCar
 
 
-  def initailize(year, color, model)
+  def initialize(year, color, model)
     @year=year
     @color=color
     @model=model
+    @current_speed=0
   end
 
-  def speedup
-    puts "Car speed up to 200km !!"
+  def speed_up(number)
+    @current_speed += number
+    puts "#{@model} speed up to #{@current_speed} km !!"
+  end
+
+  def current_speed
+    puts "#{@model} current speed is #{@current_speed} km !!"
   end
 
   def break
-    puts "Car break !!"
+    @current_speed=0
+    puts "#{@model} current speed is #{@current_speed} km !!"
   end
 
   def shut_the_car_off
@@ -23,7 +31,9 @@ end
 
 
 
-benz=MyCar.new
-benz.speedup
+benz=MyCar.new(2015, "black", "SLK500")
+benz.speed_up(300)
+benz.current_speed
+benz.speed_up(100)
 benz.break
 benz.shut_the_car_off
