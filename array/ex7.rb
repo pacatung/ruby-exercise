e0583987031e0373a9ab77b79e3e2c12874d87eb
BigBoss <pacatung@gmail.com>
2015-03-24 15:33:11 +0800

    ex7.rb more method

--- array/ex7.rb
+++ array/ex7.rb
@@ -1,15 +1,24 @@
 old_arr=[1,2,3,4,5,6,7,8,9,11110]
 
-#new_arr=[]
-#old_arr.each do |addtwo|
-#  new_arr push addtwo + 2
-#end
-
 new_arr = old_arr.map {|plusetwo| plusetwo+2}
 
-def p(x)
+def ppp(x)
   puts x
 end
 
-p(old_arr)
-p(new_arr)
+#p(old_arr)
+#p(new_arr)
+
+p old_arr
+p new_arr
+
+#new_arr=[]
+#old_arr.each do |addtwo|
+#  new_arr << addtwo + 2
+#end
+
+new1_arr=[]
+new1_arr.push(old_arr.collect {|pluseplusetwo| pluseplusetwo+2})
+
+
+ppp(new1_arr)
