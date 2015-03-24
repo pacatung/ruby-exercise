205009da5fb7efe5475c1f7f714e94b164d0bef4
BigBoss <pacatung@gmail.com>
2015-03-24 21:55:51 +0800

    first modify

--- oo_part2/classandobject_part1/ex1.rb
+++ oo_part2/classandobject_part1/ex1.rb
@@ -11,16 +11,17 @@ class MyCar
 
   def speed_up(number)
     @current_speed += number
-    puts "#{@model} speed up to #{@current_speed} km !!"
+    puts "#{@model} push the gas and accelerate #{number} km !!"
   end
 
   def current_speed
+    # @current_speed = number
     puts "#{@model} current speed is #{@current_speed} km !!"
   end
 
-  def break
-    @current_speed=0
-    puts "#{@model} current speed is #{@current_speed} km !!"
+  def break(number)
+    @current_speed -= number
+    puts "#{@model} push the break and accelerate #{number} km !!"
   end
 
   def shut_the_car_off
@@ -30,10 +31,10 @@ class MyCar
 end
 
 
-
 benz=MyCar.new(2015, "black", "SLK500")
 benz.speed_up(300)
 benz.current_speed
 benz.speed_up(100)
-benz.break
+benz.current_speed
+benz.break (100)
 benz.shut_the_car_off
